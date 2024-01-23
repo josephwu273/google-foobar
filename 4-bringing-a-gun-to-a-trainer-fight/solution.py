d58906bdb8f719dccdb1eea7342f8c94ad720a2a
josephwu273 <josephwu@buffalo.edu>
2024-01-23 00:03:35 -0500

    Successful problem 4 solution

--- 4-bringing-a-gun-to-a-trainer-fight/solution.py
+++ 4-bringing-a-gun-to-a-trainer-fight/solution.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from math import sqrt, ceil
 
 
 def gcd(a, b):
@@ -19,17 +19,19 @@ class Point:
     
     def __repr__(self):
         c = self.__class__.__name__
-        return c + str((self.x, self.y))
- 
-    def reflectX(self):
+        return c + str((self.x, self.y)) 
+
+    def reflectX(self, c=0):
+        #Reflects point acros the line x=c
         #This allows subclasses to return an instance of themself rather than of Point
-        return self.__class__(self.x,-self.y)
+        return self.__class__(2*c-self.x, self.y)
     
-    def reflectY(self):
-        return self.__class__(-self.x, self.y)
+    def reflectY(self, c=0):
+        #Reflects point acros the line x=c
+        return self.__class__(self.x, 2*c-self.y)
 
     def reflectXY(self):
-        return self.__class__(-self.x, self.y)
+        return self.__class__(-self.x, -self.y)
 
     def distanceTo(self, op):
         return sqrt((op.x-self.x)**2 + (op.y-self.y)**2)
@@ -38,7 +40,7 @@ class Point:
         x = op.x-self.x
         y = op.y-self.y
         g = gcd(x,y)
-        return [x/h, y/g]
+        return (x/g, y/g)
 
 
 class Guard(Point):
@@ -49,15 +51,68 @@ class Player(Point):
     pass
 
 
-class Room:
-    def __init__(self, xlen, ylen, player, guard):
-        self.xlen = xlen
-        self.ylen = ylen
-        self.player = player
-        self.guard = guard
-    
-    def expand_room(self):
-        foo = self.dist/xlen+1 # ceil()
-        bar = self.dist/ylen+1
-        fake_players = []
-        fake_guards = []
+def expand_room(room, player, guard, dis):
+    xlen, ylen = room
+    max_x = float(player.x+dis)
+    max_y = float(player.y+dis)
+    xrooms = int(ceil(max_x/xlen))
+    yrooms = int(ceil(max_y/ylen))
+    image_players = [[None]*yrooms for _ in range(xrooms)]
+    image_players[0][0] = player
+    image_guards = [[None]*yrooms for _ in range(xrooms)]
+    image_guards[0][0] = guard
+    for i in range(xrooms):
+        for j in range(yrooms):
+            if j==0:
+                if i==0:
+                    continue
+                else:
+                    image_players[i][j] = image_players[i-1][j].reflectX(i*xlen)
+                    image_guards[i][j] = image_guards[i-1][j].reflectX(i*xlen)
+            else:
+                image_players[i][j] = image_players[i][j-1].reflectY(j*ylen)
+                image_guards[i][j] = image_guards[i][j-1].reflectY(j*ylen)
+    targets = sum(image_players, []) + sum(image_guards, [])
+    return targets
+            
+
+def expand_quadrants(targets):
+    qii = map(lambda p: p.reflectY(), targets)
+    qiii = map(lambda p: p.reflectXY(), targets)
+    qiv = map(lambda p: p.reflectX(), targets)
+    return targets + qii + qiii + qiv
+
+
+def sort_and_filter(targets, dist, origin):
+    f = filter(lambda p : p.distanceTo(origin)<=dist, targets)
+    f.sort(key = lambda p:p.distanceTo(origin))
+    return f
+
+def get_vectors(targets, origin):
+    vectors = dict()
+    while targets:
+        t = targets.pop(0)
+        if t.distanceTo(origin)==0:
+            continue
+        else:
+            v = origin.vectorTo(t)
+            if v not in vectors:
+                vectors[v] = t
+    return vectors
+
+
+def guard_count(vectors):
+    targets = vectors.values()
+    guards = sum(isinstance(t, Guard) for t in targets)
+    return guards
+
+
+def solution(dimensions, your_position, trainer_position, distance):
+    player = Player(your_position)
+    guard = Guard(trainer_position)
+    qi_targets = expand_room(dimensions, player, guard, distance)
+    all_targets = expand_quadrants(qi_targets)
+    sf = sort_and_filter(all_targets, distance, player)
+    vectors = get_vectors(sf, player)
+    guards = guard_count(vectors)
+    return guards
